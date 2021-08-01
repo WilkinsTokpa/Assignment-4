3786353188b4a2b0fecec1d0005ba916fae39af0
WilkinsTokpa <88234682+WilkinsTokpa@users.noreply.github.com>
2021-08-01 23:48:52 +0530

    Final Project

---
+++ Assignment 4.py
@@ -0,0 +1,4 @@
+print("A Python Base Degree Fahrenheit to Celsius")
+celsius = int(input('PLEASE ENTER THE TEMPERATURE IN CELSIUS: '))
+fahrenheit = (celsius * 9/5) + 32
+print("The converted temperature is", fahrenheit, "Degree Fahrenheit")
